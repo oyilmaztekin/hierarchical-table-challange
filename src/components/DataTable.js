4e58dc69140c64ada6bd68ceab821bc55b075100
özer yılmaztekin <yilmaztekin.ozer@gmail.com>
2018-11-19 14:07:57 +0300

    fix: fixed double tbody

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -21,63 +21,61 @@ class DataTable extends Component {
       }
     }
   }
+
   removeNodeFromTree(index) {
     const users = Object.assign(this.props.context.state.users)
     this.removeNode(users, index)
     this.props.context.updateValue("users", users)
   }
 
-  toggleCollapseUser(user, users) {
+  toggleCollapseUser(user) {
+    const users = Object.assign(this.props.context.state.users)
     user.collapse = !user.collapse
     const newUsers = Object.assign(users)
     this.props.context.updateValue("users", newUsers)
   }
 
-  childrenRecursivelyRenderer(child) {
-    const newUsers = Object.assign(this.props.context.state.users)
-    if (!child.collapse) return null
-      return child.children.map((u, i) => {
-        return (
-          <React.Fragment key={i}>
-            <tbody>
-              <tr>
-                <td> {u.ID} </td>
-                <td> {u.Phone} </td>
-                <td> {u.City} </td>
-                <td> {u.Name} </td>
-                <td>
-                  {" "}
-                  {!!u.children.length && (
-                    <Button
-                      bsStyle="link"
-                      onClick={() => this.toggleCollapseUser(u, newUsers)}
-                    >
-                      Toggle Collapse{" "}
-                      <Glyphicon glyph={`chevron-${u.collapse ? 'up' : 'down'}`} />
-                    
-                    </Button>
-                  ) }
-                </td>
-                <td>
-                  <Button
-                    bsStyle="danger"
-                    bsSize="xsmall"
-                    onClick={() => this.removeNodeFromTree(u.ID)}
-                  >
-                    Remove
-                  </Button>
-                </td>
-              </tr>
-            </tbody>
-            {child.children &&
-              this.childrenRecursivelyRenderer(child.children[i])}
-          </React.Fragment>
-        )
-      })
+  childrenRecursivelyRenderer(child, i) {
+    return (
+      <React.Fragment key={i}>
+        <tbody>
+          <tr>
+            <td> {child.ID} </td>
+            <td> {child.Phone} </td>
+            <td> {child.City} </td>
+            <td> {child.Name} </td>
+            <td>
+              {!!child.children.length && (
+                <Button
+                  bsStyle="link"
+                  onClick={() => this.toggleCollapseUser(child)}
+                >
+                  Toggle Collapse{" "}
+                  <Glyphicon glyph={`chevron-${child.collapse ? 'up' : 'down'}`} />
+                </Button>
+              )}
+            </td>
+            <td>
+              <Button
+                bsStyle="danger"
+                bsSize="xsmall"
+                onClick={() => this.removeNodeFromTree(child.ID)}
+              >
+                Remove
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+        {child.children && child.collapse && (
+          child.children.map(this.childrenRecursivelyRenderer)
+        )}
+      </React.Fragment>
+    )
   }
 
   render() {
-    const newUsers = Object.assign(this.props.context.state.users)
+    const newUsers = Object.assign(this.props.context.state.users);
+
     return (
       <div className="person">
         <React.Fragment>
@@ -95,48 +93,7 @@ class DataTable extends Component {
                     <th />
                   </tr>
                 </thead>
-                {newUsers.map((user, index) => {
-                  return (
-                    <React.Fragment key={index}>
-                      <tbody>
-                        <tr>
-                          <td> {user.ID} </td>
-                          <td> {user.Phone} </td>
-                          <td> {user.City} </td>
-                          <td> {user.Name} </td>
-                          <td>
-                            {" "}
-                            {user.children.length ? (
-                              <Button
-                                bsStyle="link"
-                                onClick={() =>
-                                  this.toggleCollapseUser(user, newUsers)
-                                }
-                              >
-                                Toggle Collapse{" "}
-                                {user.collapse ? (
-                                  <Glyphicon glyph="chevron-up" />
-                                ) : (
-                                  <Glyphicon glyph="chevron-down" />
-                                )}
-                              </Button>
-                            ) : null}
-                          </td>
-                          <td>
-                            <Button
-                              bsStyle="danger"
-                              bsSize="xsmall"
-                              onClick={() => this.removeNodeFromTree(user.ID)}
-                            >
-                              Remove
-                            </Button>
-                          </td>
-                        </tr>
-                      </tbody>
-                      {this.childrenRecursivelyRenderer(user)}
-                    </React.Fragment>
-                  )
-                })}
+                {newUsers.map(this.childrenRecursivelyRenderer)}
               </Table>
             </Col>
           </Row>
